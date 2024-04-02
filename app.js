86c9795219f1bfeff282f91e9b1ed78d07476b3f
Muhammad Momin <160387789+MuhammadMomin095@users.noreply.github.com>
2024-04-02 18:44:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function logObjectProperties(obj) {
+    for (var property in obj) {
+        console.log("".concat(property, ": ").concat(obj[property]));
+    }
+}
+logObjectProperties({ make: "Toyota", model: "Corolla", year: 2021, color: "blue" });
---
+++ app.ts
@@ -0,0 +1,7 @@
+function logObjectProperties(obj: object) {
+    for (let property in obj) {
+        console.log(`${property}: ${obj[property]}`);
+    }
+}
+
+logObjectProperties({make: "Toyota", model: "Corolla", year: 2021, color: "blue"});
